d5e8a8745744c50dabc5346ca30b9e54a09ff0b2
KhalilBenKhoud <khalil.b.khoud1998@gmail.com>
2022-05-16 22:38:28 +0100

    commented index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthContextProvider } from './context/auth-context';
+//Auth provider should wrap the app
 ReactDOM.render(
 	<Router>
 		<AuthContextProvider>
